018ed5f3bfe7aae931fcbdf51b4731d96e704022
Manya Joshi <66204318+Manya-15@users.noreply.github.com>
2025-07-15 19:17:58 +0530

    Update final_api.py

--- final/final_api.py
+++ final/final_api.py
@@ -9,7 +9,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 app = Flask(__name__)
 
 # --- Gemini API Key and Model ---
-os.environ["GOOGLE_API_KEY"] = "AIzaSyC4q-ry8oPTjBHDP1suYrtB2PX52MXREwg"
+os.environ["GOOGLE_API_KEY"] = "key"
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
 parser = JsonOutputParser()
 
